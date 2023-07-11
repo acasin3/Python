ff511d0d66d307c5189e298d169b30121784301d
acasin3 <a.casin.3rd@gmail.com>
2023-07-11 22:18:47 +0800

    Initial commit.

---
+++ 03.py
@@ -0,0 +1,20 @@
+import math
+
+def is_prime(n):
+    for i in range(2, math.ceil(math.sqrt(n + 1))):
+        if n % i == 0:
+            return False
+    return True
+
+def largest_prime(n):
+    max = math.ceil(math.sqrt(n))
+    while max > 1 :
+        if n % max == 0 and is_prime(max) == True:
+            break
+        else:
+            max -= 1
+
+    return max
+
+print(largest_prime(600851475143))
+
